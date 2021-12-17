9af336a2b9b993648d49085217605ec1be105b4e
EffenrilAgung <66568136+EffenrilAgung@users.noreply.github.com>
2021-12-17 11:08:33 +0700

    completed

--- assets/script/main.js
+++ assets/script/main.js
@@ -3,6 +3,6 @@
     gsap.registerPlugin(TextPlugin);
     gsap.to('.agung', {duration: 1.5, delay:1, text: 'Blog Ini Membahas Tentang Teknologi Baik Linux, Pemograman, Serta Berisikan, Catatan Harian Seputar Teknologi Terbaru.'})
 
-    gsap.to('.rizki', {duration: 1.5, delay:1, text: 'Blog Ini Membahas Tentang Teknologi, Serta Terdapat Tutorial Web Desain, Web Development, Yang DapatMembantu Menyelesaikan Masalah dalam Dunia Web Programmer..'})
+    gsap.to('.rizki', {duration: 1.5, delay:1, text: 'Blog Ini Membahas Tentang Teknologi, Serta Terdapat Tutorial Web Desain, Web Development, Yang DapatMembantu Menyelesaikan Masalah dalam Dunia Web Programmer.'})
 
     gsap.from('.containerButton', {duration: 1.5, y: '-100%', opacity: 0, delay: 1.7, ease: 'bounce'})
